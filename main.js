de8859df9f9689bb70300320d6808e8a066e018e
rahmonali2009 <mahamadalivrahmonali@gmail.com>
2023-09-09 12:04:26 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,105 @@
+function yoz7(){
+    var inp1 = document.getElementById('inp1');
+    console.log(inp1);
+    inp1.value += "7";
+ }
+ 
+ function yoz8(){
+    var inp1 = document.getElementById('inp1');
+    inp1.value += "8";
+ }
+ 
+ function bajar9(){
+    var inp1 = document.getElementById('inp1');
+    inp1.value += "9";
+ }
+ 
+ function yozPlus(){
+     var inp1 = document.getElementById('inp1');
+     inp1.value += "+";
+ }
+ 
+ function yoz4(){
+     var inp1 = document.getElementById('inp1');
+     inp1.value += "4";
+ }
+ 
+ function yoz5(){
+     var inp1 = document.getElementById('inp1');
+     inp1.value += "5";
+ }
+ 
+ function xisobla(){
+     var inp1 = document.getElementById('inp1');
+     console.log(inp1.value);
+     var result = eval(inp1.value);
+     console.log(result);
+     inp1.value = result;
+
+ }
+function yoz61(){
+var inp1 = document.getElementById('inp1')
+inp1.value += '6';
+}
+
+
+function yoz6(){
+var inp1 = document.getElementById('inp1')
+inp1.value += '-';
+}
+
+function yoz1(){
+    var inp1 = document.getElementById('inp1')
+    inp1.value += '1';
+    }
+
+
+    function yoz2(){
+        var inp1 = document.getElementById('inp1')
+        inp1.value += '2';
+        }
+
+        function yoz3(){
+            var inp1 = document.getElementById('inp1')
+            inp1.value += '3';
+            }
+
+            
+  function yoz100(){
+   var inp1 = document.getElementById('inp1');
+ inp1.value += '*';
+   }
+
+
+
+            function yoz0(){
+                var inp1 = document.getElementById('inp1')
+                inp1.value += '0';
+                }
+
+                function yozp(){
+                    var inp1 = document.getElementById('inp1')
+                    inp1.value += '/';
+                    }
+  function yoz111(){
+   var inp1 = document.getElementById('inp1')
+ inp1.value += ',';
+  }
+
+  function yoz (){
+    var inp1 = document.getElementById('inp1')
+  inp1.value += '=';
+   }
+
+
+   function yoz (){
+    var inp1 = document.getElementById('inp1')
+  inp1.value += '/';
+   }
+
+
+
+
+
+
+
